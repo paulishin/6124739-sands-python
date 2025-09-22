b2e69e7f1305e6e608c2b88316e14396ac68070a
paulishin <P.Shinkorenko@student.tudelft.nl>
2025-09-22 17:15:36 +0200

    created singals

---
+++ signals.py
@@ -0,0 +1,5 @@
+import numpy as np
+def generate_sine_wave(frequency, duration, sample_rate):
+    t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
+    signal = np.sin(2 * np.pi * frequency * t)
+    return signal
