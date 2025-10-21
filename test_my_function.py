9c34a8d631d206e27ab134b59d7ddeb17a0b62f5
paulishin <P.Shinkorenko@student.tudelft.nl>
2025-10-22 00:23:43 +0200

    corrected code

--- test_my_function.py
+++ test_my_function.py
@@ -4,5 +4,5 @@ os.chdir("C:\\Users\\shinp\\Documents\\unif\\python\\6124739-sands-python")
 
 from signals import test_generate_sine_wave
 
-from signals import test_step_function
+from signal_2 import test_step_function
 
