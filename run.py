38cffa31df63539b624ab465859da8854d3613c2
paulishin <P.Shinkorenko@student.tudelft.nl>
2025-10-22 00:15:10 +0200

    rectified some code

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import os
 import matplotlib.pyplot as plt
 os.chdir("C:\\Users\\shinp\\Documents\\unif\\python\\6124739-sands-python")
 
+
 from signals import generate_sine_wave
 t, y = generate_sine_wave(1, 2, 1)
 print(y[:5]) 
@@ -19,15 +20,14 @@ plt.grid(True)
 plt.show()
 
 
-from signals import step_function
-y_step = step_function(t, 0.5)
-
+from signal_2 import step_function
+t_step, y_step = step_function(t, 0.5)
 shifted_t = t + shift
-y_step_shifted = step_function(shifted_t, 0.5)
+t_step_shifted, y_step_shifted = step_function(shifted_t, 0.5)
 
 plt.figure()
 plt.plot(t, y_step, label="step function")
-plt.plot(shifted_t, y_step_shifted, label="shifted step function")
+plt.plot(t, y_step_shifted, label="shifted step function")
 plt.xlabel("time")
 plt.ylabel("amplitude")
 plt.legend()
